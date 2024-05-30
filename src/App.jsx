0db448549f5fb35c57ced189c8b317829f1c0fa0
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-05-30 21:50:27 +0200

    remove incorrect components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,6 @@
 
-import { useState } from "react";
-import Match from "./components/Match"
-import NewMatchForm from "./components/NewMatchForm";
-
 function App() {
-  const [newMatch, setNewMatch] = useState(null);
-
-  const handleFormSubmit = (data) => {
-    setNewMatch(data);
-    console.log(data);
-  };
-
-  return (
-    <>
-    <NewMatchForm onFormSubmit={handleFormSubmit}/>
-    <Match newMatch={newMatch}/>
-    </>
-  )
+  
 }
 
 export default App
