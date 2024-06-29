8d15c0a4f5ef5829c7a1ec47d51e6f066150b997
unknown <stopkakrzysztof1@gmail.com>
2024-06-29 09:43:19 +0200

    Add text to navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 import Homepage from "./pages/Homepage"
+import Posts from "./pages/Posts"
 import PostDetails from "./pages/PostDetails"
 import SiteHeader from "./components/SiteHeader"
 
@@ -12,7 +13,8 @@ function App() {
     <SiteHeader />
     <Routes>
       <Route path="/" element={<Homepage/>} />
-      <Route path="/posts" element={<PostDetails/>} />
+      <Route path="/posts" element={<Posts/>} />
+      <Route path="/post:id" element={<PostDetails/>} />
     </Routes>
     </div>
     </Router>
--- frontend/src/components/SiteHeader.tsx
+++ frontend/src/components/SiteHeader.tsx
@@ -3,7 +3,16 @@ import { Link } from "react-router-dom";
 export default function SiteHeader() {
   return (
     <div>
-        <Link to="/"><h1>Home Page</h1></Link>
+        <div></div>
+        <div>
+          <Link to="/">Strona główna</Link>
+          <Link to="/">Aktualności</Link>
+          <Link to="/">Seniorzy</Link>
+          <Link to="/">Szkółka Piłkarska</Link>
+          <Link to="/">Multimedia</Link>
+          <Link to="/">Kontakt</Link>
+          </div>
+        <div></div>
     </div>
   )
 }
